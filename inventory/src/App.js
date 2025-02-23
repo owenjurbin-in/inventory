61033cfa4f6ecd77275a677aa3ebbb2e6364c730
owenjurbin-in <owenjurbin@gmail.com>
2025-02-23 14:41:37 -0500

    Add Search Bar

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar';
+import {useState} from "react"
 
 function App() {
+  const [data, setData] = useState({});
+  const updateData = (searchParams) => {
+     setData(searchParams)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar callback={updateData}/>
+      <p> Name: {"name" in data ? data["name"] : "No Data to Display"}</p>
+      <p> Max Price: {"price" in data ? data["price"] : "No Data to Display"}</p>
+      <p> Type: {"type" in data ? data["type"] : "No Data to Display"}</p>
+      <p> Brand: {"brand" in data ? data["brand"] : "No Data to Display"}</p>
     </div>
   );
 }
